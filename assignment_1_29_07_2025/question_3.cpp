56c9f05b69a2713cd14b8a037a0bd9dda0d66594
23103034-JIIT <23103034@mail.jiit.ac.in>
2025-07-29 08:40:41 +0000

    temp commit

---
+++ assignment_1_29_07_2025/question_3.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string tmp;
+    getline(cin, tmp);
+    string rev(tmp.rbegin(), tmp.rend());
+    cout << rev << endl;
+    return 0;
+}
