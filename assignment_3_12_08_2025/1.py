f4d5ebaef114c1f83cd3f6dda3e4428f2f492d8a
23103034 <23103034@pdc.jiit>
2025-08-12 13:45:33 +0530

    Question 1 solved

---
+++ assignment_3_12_08_2025/1.py
@@ -0,0 +1,11 @@
+
+def invertdic(k):
+	return {v:k fro k, v in t.items()}
+
+dic = {
+	1 : 2,
+	3 : 4
+}
+inverted = invertdic(dic)
+print("Original Dictionary: ", dic)
+print("Inverted Dictionary: ", inverted)
